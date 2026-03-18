095bf0ef4fd6178b77ae0c33481c4a87b31cebf6
mscarn02 <154599559+mscarn02@users.noreply.github.com>
2026-03-18 15:20:20 +0530

    Update test_lambda.py

--- test_lambda.py
+++ test_lambda.py
@@ -5,12 +5,12 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "lambda"))
 
 from src.handlers.s3_healer import lambda_handler
 
-os.environ['DISCORD_WEBHOOK_URL'] = "https://discord.com/api/webhooks/1481963820258820240/9_rElfeai0-C4uneHrffCxTirslHfn4eYLPzPEAG5hUFr2LZ9N84yCAX8hH1EyahvSNv"
+os.environ['DISCORD_WEBHOOK_URL'] = #YOUR URL HERE
 
 test_event = {
     "detail": {
         "requestParameters": {
-            "bucketName": "scarn-test-bucket"
+            "bucketName": " " #input your bucket name here
         }
     }
 }
